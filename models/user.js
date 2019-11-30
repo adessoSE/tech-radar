4548216a3242411b9ce421d88258c61d85e76d67
Leonie Dannemann <leonie.dannemann@web.de>
2019-11-30 17:05:42 +0100

    - add user.js for mapping to database

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+    name : String,
+    email: String,
+    passwort: String,
+    rolle: String,
+});
+
+mongoose.model('user', userSchema);
+
+//ToDO Passwort eventuell verschlüsselt speichern?? Rolle als enum (User/Admin)
