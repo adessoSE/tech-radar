c033778f2da881d3af7385b20b0b70aef37da54a
Leonie Dannemann <leonie.dannemann@web.de>
2019-12-05 11:40:15 +0100

    - add Endpoint for all comments

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -8,6 +8,11 @@ module.exports = (app) => {
         return res.status(200).send(comments);
     });
 
+    app.get('api/comment/', async (req, res) => {
+        let comments = await Comment.find();
+        return res.status(200).send(comments);
+    });
+
     app.post(`/api/comment`, async (req, res) => {
         let comment = await Comment.create(req.body);
         return res.status(201).send({
