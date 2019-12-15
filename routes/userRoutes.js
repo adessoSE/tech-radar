bdc16002fc8e54d63c0cda4a08782fe5e6fd8b60
Nadja07 <nadink93@gmail.com>
2019-12-15 14:31:09 +0100

    wip:setting CORS to all routes+change some code to check the user's credentials

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
+
 module.exports = (app) => {
 
-    app.get('/api/user/', async (req, res) => {
+    app.get('/api/user/',async (req, res) => {
         let users = await User.find({
             email: req.query.email,
             passwort: req.query.passwort
         });
         return res.status(200).send(users);
     });
-
+    app.post(`/api/user`, async (req, res) => {
+        let user = await User.findOne(req.body);
+        return res.status(200).send({
+            error: false,
+            user
+        })
+    })
 };
 
