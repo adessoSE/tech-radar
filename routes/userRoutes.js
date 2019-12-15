6d1532d3e27cd10ba725e26721083c867f720090
Nadja07 <nadink93@gmail.com>
2019-12-15 23:22:23 +0100

    wip:auth check

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,6 @@ module.exports = (app) => {
     app.post(`/api/user`, async (req, res) => {
         let user = await User.findOne(req.body);
         return res.status(200).send({
-            error: false,
             user
         })
     })
