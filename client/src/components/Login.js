faabd00083a3dc68e173df0556367329437e3a5f
Nadja07 <nadink93@gmail.com>
2019-12-15 23:22:23 +0100

    wip:auth check

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,7 +5,15 @@ import "../static/css/desctop.scss";
 import "../static/css/mobile.scss";
 import "./Login.scss"
 import userService from "../services/userService";
-
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useHistory,
+    useLocation
+} from "react-router-dom";
 
 class Login extends Component {
 
@@ -13,57 +21,79 @@ class Login extends Component {
         super(props)
 
         this.state = {
-            errors: {},
-            formSubmitted: false,
             email: '',
             passwort: '',
+            isAuthenticated: false
 
         }
-        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleEmailChanged = this.handleEmailChanged.bind(this);
+        this.handlePasswortChanged = this.handlePasswortChanged.bind(this);
         this.verifyUserInput = this.verifyUserInput.bind(this);
+
+
     }
 
-    handleInputChange = (event) => {
-        let name = event.target.name;
-        let value = event.target.value;
-        this.setState({[name]: value});
-        //console.log(this.state.email);
-        //console.log(this.state.passwort);
+    handleEmailChanged(event) {
+        this.setState({email: event.target.value});
+
+
     }
 
+    handlePasswortChanged(event) {
+        this.setState({passwort: event.target.value});
 
+    }
 
     verifyUserInput = async (event) => {
-        console.log(this.state.email);
-        console.log(this.state.passwort);
         event.preventDefault();
         const datatest = await userService.submitUser(this.state.email, this.state.passwort);
-        if(datatest===[]) {
-
-        }
         console.log(datatest);
+        localStorage.setItem('email', JSON.stringify(this.state.email));
+        localStorage.setItem('password', JSON.stringify(this.state.password));
+
+        if (datatest.user != null) {
+
+            //console.log("success"),
+            this.setState({isAuthenticated:true})
+            console.log(this.state.isAuthenticated)
+            this.props.history.push('/');
+        }
+
+        else {
+            this.setState({isAuthenticated:false})
+            console.log(this.state.isAuthenticated)
+            console.log("not working")
+        }
     };
 
+
+
     render() {
-        return (
-            <div className="Login">
-                <Row>
-                    <form onSubmit={this.login}>
-                        <FormGroup controlId="email">
-                            <FormLabel>E-mail</FormLabel>
-                            <input type="email" name="email" placeholder="email" onChange={this.handleInputChange}/>
-                        </FormGroup>
-                        <FormGroup controlId="passwort">
-                            <FormLabel>Passwort</FormLabel>
-                            <FormControl type="password" name="passwort" placeholder="passwort"
-                                         onChange={this.handleInputChange}/>
-                        </FormGroup>
-                        <Button type="submit" onClick={this.verifyUserInput} bsstyle="primary">Log In</Button>
-                    </form>
-                </Row>
-            </div>
-        )
+
+        return(
+          <div>
+              {this.isAuthenticated? <Redirect to={{pathname:'/'}}/> :
+                  (
+
+                          <form>
+                              <FormGroup controlId="email">
+                                  <FormLabel>E-mail</FormLabel>
+                                  <input type="email" name="email" placeholder="email" onChange={this.handleEmailChanged}/>
+                              </FormGroup>
+                              <FormGroup controlId="passwort">
+                                  <FormLabel>Passwort</FormLabel>
+                                  <FormControl type="password" name="passwort" placeholder="passwort"
+                                               onChange={this.handlePasswortChanged}/>
+                              </FormGroup>
+                              <Button type="submit" onClick={this.verifyUserInput} bsstyle="primary">Log In</Button>
+                          </form>
+
+                      )
+              }
+          </div>
+        );
     }
 }
+
 
 export default Login;
