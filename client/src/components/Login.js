e6b88f074a5e3604b1cfda154234dde479c3fcf1
Leonie Dannemann <leonie.dannemann@web.de>
2019-12-19 09:45:36 +0100

    - Login check if/else loginSuccess

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { FormGroup, FormControl, Button} from 'react-bootstrap';
+import {FormGroup, FormControl, Button} from 'react-bootstrap';
 import "../static/css/styles.scss";
 import "../static/css/desctop.scss";
 import "../static/css/mobile.scss";
@@ -14,8 +14,7 @@ class Login extends Component {
         this.state = {
             email: '',
             passwort: '',
-
-
+            loginSuccess: '',
         }
         this.handleEmailChanged = this.handleEmailChanged.bind(this);
         this.handlePasswortChanged = this.handlePasswortChanged.bind(this);
@@ -26,7 +25,6 @@ class Login extends Component {
 
     handleEmailChanged(event) {
         this.setState({email: event.target.value});
-
 
     }
 
@@ -40,36 +38,38 @@ class Login extends Component {
         const datatest = await userService.submitUser(this.state.email, this.state.passwort);
         console.log(datatest);
 
-
         if (datatest.user != null) {
             localStorage.setItem('email', JSON.stringify(this.state.email));
             this.props.history.push('/');
-        }
-
-        else {
+            this.setState({loginSuccess: true})
+        } else {
+            this.setState({loginSuccess: false})
             console.log("no matching")
         }
     };
 
 
     render() {
-
-        return(
+        let error='';
+        if (this.state.loginSuccess === false) {
+            error = (<div>Username oder Passwort nicht korrekt</div>);
+        }
+        return (
             <div className="Login">
-                        <form onSubmit={this.verifyUserInput} >
-                            <h3 align="center" >Willkommen beim Adesso TechnologieRadar</h3>
-                            <FormGroup controlId="email">
-                                <input type="email" name="email" placeholder="email" onChange={this.handleEmailChanged}/>
-                            </FormGroup>
-                            <FormGroup controlId="passwort">
-                                <FormControl type="password" name="passwort" placeholder="passwort"
-                                             onChange={this.handlePasswortChanged}/>
-                            </FormGroup>
-                            <FormGroup controlId="submit">
-                                <Button type="submit" onClick={this.verifyUserInput} bsstyle="primary">Einloggen</Button>
-                            </FormGroup>
-                        </form>
-                }
+                <form onSubmit={this.verifyUserInput}>
+                    <h3 align="center">Willkommen beim Adesso TechnologieRadar</h3>
+                    <div>{error}</div>
+                    <FormGroup controlId="email">
+                        <input type="email" name="email" placeholder="email" onChange={this.handleEmailChanged}/>
+                    </FormGroup>
+                    <FormGroup controlId="passwort">
+                        <FormControl type="password" name="passwort" placeholder="passwort"
+                                     onChange={this.handlePasswortChanged}/>
+                    </FormGroup>
+                    <FormGroup controlId="submit">
+                        <Button type="submit" onClick={this.verifyUserInput} bsstyle="primary">Einloggen</Button>
+                    </FormGroup>
+                </form>
             </div>
         );
     }
