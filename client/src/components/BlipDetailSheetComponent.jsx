1c93ee7eff4dec5106ff9de7d46bec3f7bfac236
Frieda Gruse <fr.ida@gmx.de>
2020-01-08 22:28:33 +0100

    balken doch immer sichtbar

--- client/src/components/BlipDetailSheetComponent.jsx
+++ client/src/components/BlipDetailSheetComponent.jsx
@@ -242,7 +242,7 @@ class BlipDetailSheetComponent extends React.Component {
 
     getBalken() {
         let balken = <div className="balken"></div>;
-        if (this.props.ring === "einsetzen" && (!(this.getCount(4) === 0 && this.getCount(1) === 0 && this.getCount(2) === 0)) && this.getTotalCount() >= 10) {
+        if (this.props.ring === "einsetzen" && (!(this.getCount(4) === 0 && this.getCount(1) === 0 && this.getCount(2) === 0))) {
             balken = (<div className="balken">
                 <div className="innen tooltip" style={this.getStyle(4)}>{this.getCount(4)}<span
                     className="tooltiptext">In Einsetzen belassen</span></div>
@@ -254,7 +254,7 @@ class BlipDetailSheetComponent extends React.Component {
                 </div>
             </div>);
 
-        } else if (this.props.ring === "evaluieren" && (!(this.getCount(5) === 0 && this.getCount(2) === 0 && this.getCount(3) === 0)) && this.getTotalCount() >= 10) {
+        } else if (this.props.ring === "evaluieren" && (!(this.getCount(5) === 0 && this.getCount(2) === 0 && this.getCount(3) === 0))) {
             balken = (<div className="balken">
                 <div className="innen tooltip" style={this.getStyle(5)}>{this.getCount(5)}<span
                     className="tooltiptext">In Evaluieren belassen</span></div>
@@ -263,7 +263,7 @@ class BlipDetailSheetComponent extends React.Component {
                 <div className="außen tooltip" style={this.getStyle(3)}>{this.getCount(3)}<span
                     className="tooltiptext">Nach Einsetzen verschieben</span></div>
             </div>);
-        } else if (this.props.ring === "überdenken" && (!(this.getCount(6) === 0 && this.getCount(1) === 0 && this.getCount(3) === 0)) && this.getTotalCount() >= 10) {
+        } else if (this.props.ring === "überdenken" && (!(this.getCount(6) === 0 && this.getCount(1) === 0 && this.getCount(3) === 0))) {
             balken = (<div className="balken">
                 <div className="innen tooltip" style={this.getStyle(6)}>{this.getCount(6)}<span
                     className="tooltiptext">In Überdenken belassen</span></div>
